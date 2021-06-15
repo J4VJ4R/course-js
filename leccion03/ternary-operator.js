ee39a1e9eb952b35c547cb860dbb41ef1d759575
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-06-15 16:12:04 -0500

    it was create exercise 3

---
+++ leccion03/ternary-operator.js
@@ -0,0 +1,16 @@
+let result = (385 % 2 == 0) ? "es par" : "es impar";
+console.log(result)
+
+let miNumero = "18d"
+
+let edad = Number(miNumero)
+
+if(isNaN(edad)){
+  console.log('no es un múmero')
+}else{
+  let voto = (edad >= 18) ? "puede votar" : "no puede votar";
+}
+
+console.log(voto)
+
+//verificando que una variable sea de tipo número
