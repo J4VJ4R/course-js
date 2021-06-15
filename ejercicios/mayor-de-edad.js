fd6d092b9e97b0912f2847ce3f7043e762d82dbf
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-06-15 15:18:06 -0500

    it was create exercise 1 and 2

---
+++ ejercicios/mayor-de-edad.js
@@ -0,0 +1,9 @@
+
+function mayorDeEdad(edad){
+  if(edad < 18){
+    console.log('eres menor de edad')
+  }else{
+    console.log('eres mayor de edad')
+  }
+}
+mayorDeEdad(33)
---
+++ ejercicios/numero-par.js
@@ -0,0 +1,9 @@
+let a = 5
+
+if(a % 2 === 0){
+  console.log('el numero es par')
+}else if(a % 2 !== 0){
+  console.log('el número es impar')
+}else{
+  console.log('esto no es un número')
+}
