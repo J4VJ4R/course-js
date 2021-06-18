79f4fdb62672d9f789f7c66489867b2bb728e441
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-06-18 18:03:39 -0500

    example with a arrow functions

---
+++ leccion07/arrow-function.js
@@ -0,0 +1,3 @@
+const division = (a, b) => console.log(a / b)
+
+result = division(20, 5)
--- leccion07/expresion-function.js
+++ leccion07/expresion-function.js
@@ -1,3 +1,4 @@
 (function(a, b){
+  console.log(arguments.length)
   console.log(`la suma de ${a} + ${b} es ${a + b}`)
 })(3, 4)
